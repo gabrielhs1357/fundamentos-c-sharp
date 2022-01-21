05c8c0f01602c30d8d01aec0571ffb07234657d9
gabrielhs1357 <gabrielhs1357@gmail.com>
2022-01-21 10:25:58 -0300

    finished projects

---
+++ Calculator/Program.cs
@@ -0,0 +1,82 @@
+﻿Menu();
+
+static void Menu()
+{
+    Console.Clear();
+
+    Console.WriteLine("O que deseja fazer?");
+    Console.WriteLine("1 - Soma");
+    Console.WriteLine("2 - Subtração");
+    Console.WriteLine("3 - Divisão");
+    Console.WriteLine("4 - Multiplicação");
+    Console.WriteLine("5 - Sair");
+
+    Console.WriteLine("Selecione uma opção: ");
+    short res = short.Parse(Console.ReadLine());
+
+    switch (res)
+    {
+        case 1: Soma(); break;
+        case 2: Subtracao(); break;
+        case 3: Divisao(); break;
+        case 4: Multiplicacao(); break;
+        case 5: System.Environment.Exit(0); break;
+        default: Menu(); break;
+    }
+}
+
+static void Soma()
+{
+    Console.WriteLine("Primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Soma: {v1 + v2}");
+
+    Console.ReadKey();
+    Menu();
+}
+
+static void Subtracao()
+{
+    Console.WriteLine("Primeiro valor: ");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor: ");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Subtração: {v1 - v2}");
+
+    Console.ReadKey();
+    Menu();
+}
+
+static void Divisao()
+{
+    Console.WriteLine("Primeiro valor:");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Divisão: {v1 / v2}");
+
+    Console.ReadKey();
+    Menu();
+}
+
+static void Multiplicacao()
+{
+    Console.WriteLine("Primeiro valor:");
+    float v1 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine("Segundo valor:");
+    float v2 = float.Parse(Console.ReadLine());
+
+    Console.WriteLine($"Divisão: {v1 * v2}");
+
+    Console.ReadKey();
+    Menu();
+}
